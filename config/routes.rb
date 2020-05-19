128c6e92f9e812f8e207104da15ac175a7299d9b
flucas97 <feijolucas1997@gmail.com>
2020-05-19 00:14:39 -0300

    fix root path route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   resources :notes
   devise_for :users
   devise_scope :user do
-    root 'devise/sessions#new'
+    authenticated :user do
+      root 'notes#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
