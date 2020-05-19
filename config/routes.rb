d3b134fddaca2c3b0fe5d19915a8abdf85b85819
flucas97 <feijolucas1997@gmail.com>
2020-05-19 00:13:08 -0300

    configure login devise as root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :notes
   devise_for :users
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
