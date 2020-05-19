fba3e1732d1947c3b3542b6c99ca087eaacf1884
flucas97 <feijolucas1997@gmail.com>
2020-05-19 18:19:27 -0300

    configure Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module NotesApp
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    config.active_job.queue_adapter = :sidekiq
+
     config.generators do |g|
       g.test_framework :rspec,
       fixtures: false,
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :notes
   devise_for :users
+
   devise_scope :user do
     authenticated :user do
       root 'notes#index', as: :authenticated_root
     end
-  
+    
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount Sidekiq::Web => '/sidekiq'
 end
